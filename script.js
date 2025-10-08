e87ebcf1f9b57341d5036a72844ad6408bf60107
Ivan Spirandelli <ivan@spirandelli.de>
2025-10-07 18:19:57 -0700

    style js update

--- script.js
+++ script.js
@@ -5,12 +5,22 @@ document.addEventListener("DOMContentLoaded", function() {
     for (i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function() {
             this.classList.toggle("active");
+            
+            // --- CHANGED PART ---
             var content = this.nextElementSibling;
-            if (content.style.maxHeight){
-                content.style.maxHeight = null;
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-            } 
+            // Loop until we find the 'content' div
+            while (content && !content.classList.contains('content')) {
+                content = content.nextElementSibling;
+            }
+            // --- END OF CHANGE ---
+
+            if (content) { // Make sure we found the content div
+                if (content.style.maxHeight){
+                    content.style.maxHeight = null;
+                } else {
+                    content.style.maxHeight = content.scrollHeight + "px";
+                } 
+            }
         });
     }
 });
